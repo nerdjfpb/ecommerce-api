83860576dffaa4b9b1657888ec3d8728ea47345c
Muhammad Ali <raju4uiu@gmail.com>
2019-02-22 23:34:13 +0600

    CUstomised Exception Trait

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,13 +3,13 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
 
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -49,19 +49,23 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if($request->expectsJson()){
-            if($exception instanceof ModelNotFoundException){
-                return response()->json([
-                   'errors' =>'Product Model not found'
-                ],Response::HTTP_NOT_FOUND);
-            }
+        if($request->expectsJson()) {
+            return $this->apiException($request,$exception);
         }
 
-        if($exception instanceof NotFoundHttpException){
-            return response()->json([
-                'errors' =>'Incorrect Route'
-            ],Response::HTTP_NOT_FOUND);
-        }
+//        if($request->expectsJson()){
+//            if($exception instanceof ModelNotFoundException){
+//                return response()->json([
+//                   'errors' =>'Product Model not found'
+//                ],Response::HTTP_NOT_FOUND);
+//            }
+//        }
+//
+//        if($exception instanceof NotFoundHttpException){
+//            return response()->json([
+//                'errors' =>'Incorrect Route'
+//            ],Response::HTTP_NOT_FOUND);
+//        }
 //        dd($exception);
         return parent::render($request, $exception);
     }
